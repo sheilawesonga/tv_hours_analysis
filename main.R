b41a500e5b41d14bc5e0b3a220a117d769fd62fc
sheilawesonga <167171073+sheilawesonga@users.noreply.github.com>
2024-04-16 10:45:07 +0300

    filtered to those 30 and above

--- main.R
+++ main.R
@@ -3,8 +3,10 @@ library(here)
 
 
 tv_hours_table <- gss_cat %>% 
+  filter(age >=30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+  
 
 
 write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
